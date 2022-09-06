c849aa532a3d5bb8f908b76b2de21314abe09fdc
Jaque Martins <jaque.a.martins97@gmail.com>
2022-09-06 16:59:49 -0300

    itens orçamento e toggle das perguntas

--- js/script.js
+++ js/script.js
@@ -13,3 +13,38 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// ATIVAR ITENS NO ORÇAMENTO
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
+
+//TOGGLE PERGUNTAS FREQUENTES
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+console.log(perguntas);
